2a397993c276404ab47556bb58e92ff51619369d
Jonathan A <jonathanxl1@hotmail.com>
2021-06-30 08:46:55 -0500

    Configuracion de grafica line chart

--- src/static/js/contexts.js
+++ src/static/js/contexts.js
@@ -1,1 +1,8 @@
-export const plotByGenderCtx = document.getElementById('plot-by-gender').getContext('2d')
+
+const plotByGenderCtx = document.getElementById('plot-by-gender').getContext('2d');
+const plotByYearCtx = document.getElementById('plot-by-year').getContext('2d');
+
+export default {
+    plotByGenderCtx:plotByGenderCtx,
+    plotByYearCtx:plotByYearCtx
+}
