15ba2669decf757590010a93361bd62979cfca45
Leonard Saumeth <lsaumeth@uniempresarial.edu.co>
2021-06-29 23:34:01 -0500

    CALL HTTP REQUEST AND ADD GRAPHICS af660ab

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -14,46 +14,54 @@ const endpoint = "https://raw.githubusercontent.com/datasketch/frontend-dev-test
 $(document).ready(function()
 {
   // TODO: Haz una petición HTTP al endpoint declarado para obtener los datos
-  $.getJSON(endpoint, function(result)
-  {
+  axios({
+    method: 'GET',
+    url: endpoint
+  }).then(res => {
+    const datosJSON = res.data;
+
     // TODO: Actualiza el HTML con el número de líderes sociales asesinados
-    $(contador).text(result.length -1)
-    // TODO: Lee la documentación de Chart.js y actualiza las propiedades marcadas con FIXME en el snippet para tener un bar chart de líderes sociales asesinados por género
+    $(contador).text(datosJSON.length -1)
     
-    //Contadores Para agrupar datos
-    let contadorMaculino= 0;
-    let contadorFemenino= 3;
-    let year2016 = 0;
-    let year2017 = 0;
-    let year2018 = 0;
-    let year2019 = 0;
-    let year2020 = 0;
-    let sinFecha = 1;
+    //Arreglos contenedores de los datos
+    let genders=[0,3];
+    let years=[1,0,0,0,0,0];
+    let yearArray=[];
     
     //Bucles para hacer el recorrido y asiganar datos a los contadores
-    for(let i=0;i<result.length;i++){
-      if(result[i]['genero']==="Masculino"){
-        contadorMaculino++;
-      }else if(result[i]['genero']==="Femenino"){
-        contadorFemenino++;
+    for(let i=0;i<datosJSON.length;i++){
+      switch(datosJSON[i]['genero']){
+        case 'Masculino':
+          genders[0]++;
+          break;
+        case "Femenino":
+          genders[1]++;
+          break;
       }
     }
 
-    for(let i=0;i<result.length-2;i++){
-      var yearArray=result[i]['fecha'].split('-');
-      if(yearArray[0]=="2016"){
-        year2016++;
-      }else if(yearArray[0]=="2017"){
-        year2017++;
-      }else if(yearArray[0]=="2018"){
-        year2018++;
-      }else if(yearArray[0]=="2019"){
-        year2019++;
-      }else if(yearArray[0]=="2020"){
-        year2020++;
+    for(let i=0;i<datosJSON.length-2;i++){
+      yearArray=datosJSON[i]['fecha'].split('-');
+      switch(yearArray[0]){
+        case '2016':
+          years[1]++;
+          break;
+        case '2017':
+          years[2]++;
+          break;
+        case '2018':
+          years[3]++;
+          break;
+        case '2019':
+          years[4]++;
+          break;
+        case '2020':
+          years[5]++;
+          break;
       }
     }
 
+    // TODO: Lee la documentación de Chart.js y actualiza las propiedades marcadas con FIXME en el snippet para tener un bar chart de líderes sociales asesinados por género
 
     //Creación de las gráficas
 
@@ -69,7 +77,7 @@ $(document).ready(function()
           {
             label: 'Número de líderes asesinados por género',
             // FIXME: Actualiza esta propiedad -- Actualizado
-            data: [contadorMaculino,contadorFemenino],
+            data: [genders[0],genders[1]],
             backgroundColor: ['#086788','#FF5733']
           }
         ]
@@ -90,13 +98,12 @@ $(document).ready(function()
           {
             label: 'Número de líderes asesinados por año',
             // FIXME: Actualiza esta propiedad -- Actualizado
-            data: [sinFecha,year2016,year2017,year2018,year2019,year2020],
+            data: [years[0],years[1],years[2],years[3],years[4],years[5]],
             backgroundColor: ['#086788','#07A0C3','#f0c808','#ef798a','#855AFF','#5AFF5F']
           }
         ]
       }
     })
-
   })
 })
 
