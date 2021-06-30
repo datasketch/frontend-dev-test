1d9f0a6ee51b2786491de991ab9d729a8a56fb2f
Jonathan A <jonathanxl1@hotmail.com>
2021-06-30 06:08:16 -0500

    Recepcion de datos Json y conteo de lideres

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -4,9 +4,31 @@ import { plotByGenderCtx } from './contexts'
 const endpoint = "https://raw.githubusercontent.com/datasketch/frontend-dev-test/master/data/lideres-sociales.json"
 
 // TODO: Haz una petición HTTP al endpoint declarado para obtener los datos
+let json_ls;
 
+await fetch(endpoint)
+  .then(response => response.json())
+  .then(data=>json_ls= data)
+  .catch(e=>{
+    console.error(e);
+  })
+  // console.log(json_ls); Data tipo json
+  // console.log( Object.keys(json_ls).length); Total de datos recibidos
 
 // TODO: Actualiza el HTML con el número de líderes sociales asesinados
+//Limpieza de datos vacios en el objeto
+function clean(obj) {
+  for (var propName in obj) {
+    if (obj[propName] === null || obj[propName] === undefined || Object.keys(obj[propName]).length === 0) {
+      delete obj[propName];
+    }
+  }
+  return obj
+}
+
+let clean_ls = clean(json_ls);
+let count_ls = Object.keys(clean_ls).length;
+console.log(count_ls);
 
 
 // TODO: Lee la documentación de Chart.js y actualiza las propiedades marcadas con FIXME en el snippet para tener un bar chart de líderes sociales asesinados por género
