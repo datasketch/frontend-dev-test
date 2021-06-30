1204a848ead315777427a2dbb040ae54065dd1ac
Jonathan A <jonathanxl1@hotmail.com>
2021-06-30 07:03:53 -0500

    Id para conteo de lideres(html) y Fixme con Chartjs por genero

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -12,7 +12,7 @@ await fetch(endpoint)
   .catch(e=>{
     console.error(e);
   })
-  // console.log(json_ls); Data tipo json
+  console.log(json_ls); //Data tipo json
   // console.log( Object.keys(json_ls).length); Total de datos recibidos
 
 // TODO: Actualiza el HTML con el número de líderes sociales asesinados
@@ -30,19 +30,47 @@ let clean_ls = clean(json_ls);
 let count_ls = Object.keys(clean_ls).length;
 console.log(count_ls);
 
+let count_header = document.getElementById("count_ls");
+count_header.innerHTML = count_ls;
+
+
 
 // TODO: Lee la documentación de Chart.js y actualiza las propiedades marcadas con FIXME en el snippet para tener un bar chart de líderes sociales asesinados por género
 
-/* const plotByGenderChart = new Chart(plotByGenderCtx, {
+function counter_gender(obj) {
+  let [count_man,count_woman,count_trans] = Array(3).fill(0);
+  for (var propName in obj) {
+    if (obj[propName].genero === 'Masculino' ) {
+      count_man++;
+    }else if (obj[propName].genero === 'Femenino'){
+      count_woman++;
+    }else if (obj[propName].genero === 'Transgénero'){
+      count_trans++;
+    }
+  }
+  return {
+    'Masculino':count_man,
+    'Femenino':count_woman,
+    'Transgénero':count_trans
+  };
+}
+
+let count_gender = counter_gender(json_ls);
+console.log("Conteo por genero",count_gender);
+
+
+
+
+ const plotByGenderChart = new Chart(plotByGenderCtx, {
   type: 'bar',
   data: {
     // FIXME: Actualiza esta propiedad
-    labels: [],
+    labels: ['Masculino','Femenino','Transgénero'],
     datasets: [
       {
         label: 'Líderes sociales asesinados por género',
         // FIXME: Actualiza esta propiedad
-        data: [],
+        data: [count_gender['Masculino'],count_gender['Femenino'],count_gender['Transgénero']],
         backgroundColor: [
           '#086788',
           '#07A0C3',
@@ -52,7 +80,7 @@ console.log(count_ls);
       }
     ]
   }
-}) */
+}) 
 
 
 // TODO: Siguiendo la misma lógica, haz un line chart que muestre el número de líderes sociales asesinados por año
