5d46aecc9153356834e62c420e6ef60a7875a322
Albeiro Marin <xlandru@hotmail.com>
2021-06-29 19:14:26 -0500

    Create second chart without data

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,5 +1,6 @@
 import Chart from 'chart.js/auto'
-import { plotByGenderCtx } from './contexts'
+import { plotByGenderCtx, plotByYearCtx } from './contexts'
+
 
 const endpoint = "https://raw.githubusercontent.com/datasketch/frontend-dev-test/master/data/lideres-sociales.json"
 
@@ -24,16 +25,17 @@ window.addEventListener("load", onPageLoad);
 async function onPageLoad () {
   //modifying the liders amount contained in the html <H1> tag
   const lidersNumber = document.getElementsByClassName("section-title")[0];
-  await fetchSocialLiders();
   lidersNumber.innerHTML = lidersArray.length -1;
   //extracting the amount of liders by genres
   let genres = {male: 0, female: 0, transgender: 0};
   genres.male = await lidersArray.filter(lider => lider.genero === "Masculino").length;
   genres.female = await lidersArray.filter(lider => lider.genero === "Femenino").length;
   genres.transgender = await lidersArray.filter(lider => lider.genero === "Transgénero").length;
-  return genres;
+  //extracting the years where the lidders were killed in
+  //extracting the  kills amount per year
+  return {genres};
 };
-const genres = await onPageLoad();
+const {genres} = await onPageLoad();
 
 // TODO: Lee la documentación de Chart.js y actualiza las propiedades marcadas con FIXME en el snippet para tener un bar chart de líderes sociales asesinados por género
 
@@ -60,3 +62,42 @@ const plotByGenderChart = new Chart(plotByGenderCtx, {
 
 
 // TODO: Siguiendo la misma lógica, haz un line chart que muestre el número de líderes sociales asesinados por año
+
+
+const plotByYearChart = new Chart(plotByYearCtx, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: 'Líderes sociales asesinados por año',
+        data: [],
+        borderColor: ["#348F6C"],
+        borderWidth: 4,
+      },
+    ],
+  },
+  options: {
+    
+    scales: {
+      y: {
+        beginAtZero: true,
+        max: 20,
+      },
+    },
+    elements: {
+      point: {
+        radius: 7,
+        pointStyle: "cros",
+        backgroundColor: "#46C292",
+        hoverRadius: 10,
+        hoverBorderWidth: 3
+      },
+      line: {
+        tension: 0,
+        backgroundColor: "rgb(70, 194, 146, 0.5)",
+        fill: true
+      }
+    },
+  },
+}); 
